22028f30c849d792ee4cae8a42b6f48d2880f2fb
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-03-19 16:53:01 -0600

    Stream Code Test

--- html/index.php
+++ html/index.php
@@ -1,3 +1,15 @@
+<html>
+<body>
+<script type='text/javascript' src='swfobject.js'></script>
+<div id='mediaspace'>Playing FLV</div>
+<object width="400" height="409" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0">
+  <param name="allowscriptaccess" value="always"></param>
+  <param name="file" value="cocaine">  
+  <embed height="500" width="650" flashvars="file=cocaine&amp;streamer=rtmp://lucasdagamerhd.tv:8080/flvplayback&amp;rtmp.subscribe=true&amp;quality=best&amp;controlbar=bottom&amp;rtmp.tunneling=false&amp;frontcolor=#fff&amp;backcolor=#000&amp;stretching=exactfit&amp;autostart=true" wmode="transparent" allowfullscreen="true" allowscriptaccess="always" quality="high" src="http://s.zuuk.net/ply.swf" type="application/x-shockwave-flash"/> 
+</object>
+</body>
+</html>
+
 <?php
 
 echo "<h1>HELLO</h1>";
