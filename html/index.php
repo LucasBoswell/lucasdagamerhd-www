87d33df8fc4570fade0e6171407ed3d319246888
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-03-20 04:23:05 -0600

    Autoplay.;

--- html/index.php
+++ html/index.php
@@ -4,7 +4,7 @@
   <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
   <script src="http://vjs.zencdn.net/5.8.0/video.js"></script>
 </head>
-<video id="my_video" class="video-js" controls preload="auto" width="640" height="268" data-setup='{}'>
+<video id="my_video" class="video-js" controls preload="auto" width="640" height="268" data-setup='{"loop": "true", "autoplay": true, "preload": "true"}'>
     <source src="rtmp://lucasdagamerhd.tv/live/test" type='rtmp/mp4'>
 	<p class="vjs-no-js">
       To view this video please enable JavaScript, and consider upgrading to a web browser that
