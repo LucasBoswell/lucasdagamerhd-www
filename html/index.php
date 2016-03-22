5bd0d3d9bef6fb6d206e1aa2fa1b5010e3420661
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-03-22 07:19:26 -0600

    Test poster.;

--- html/index.php
+++ html/index.php
@@ -4,10 +4,11 @@
   <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
   <script src="http://vjs.zencdn.net/5.8.0/video.js"></script>
 </head>
-<video id="my_video" class="video-js" controls preload="auto" width="720" height="480" poster="poster.jpg" data-setup='{"loop": "true", "autoplay": true, "preload": "true"}'>
+<video id="my_video" class="video-js" controls preload="auto" width="720" height="480" data-setup='{"loop": "true", "autoplay": true, "preload": "true"}'>
     <source src="rtmp://lucasdagamerhd.tv/live/test" type='rtmp/mp4'>
 	<p class="vjs-no-js">
       To view this video please enable JavaScript, and consider upgrading to a web browser that
       <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
     </p>
+	<img src="http://video-js.zencoder.com/oceans-clip.png" width="720" height="480"/>
   </video>
