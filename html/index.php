c855cae5af81b3d120cd4a59e9a8e49c929b0b0a
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-03-26 13:41:37 -0600

    Signed in as stuffs

--- html/index.php
+++ html/index.php
@@ -46,12 +46,15 @@
 	// The ID token you need to pass to your backend:
 	var id_token = googleUser.getAuthResponse().id_token;
 	console.log("ID Token: " + id_token);
+	
+	document.getElementById('signedInAsName').innerText = "Signed in: " + profile.getName();
   };
   function signOut() {
 	var auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
 	  console.log('User signed out.');
 	});
+	document.getElementById('signedInAsName').innerText = "Not signed in";
   }
 </script>
 
@@ -62,6 +65,7 @@
 		<div class="shell">
 			<!-- Header -->
 			<div id="header">
+				<div id="signedInAsName">Not signed in</div>
 			    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
 				<a href="" onclick="signOut();">Sign out</a>
 				<div class="cl"></div>
