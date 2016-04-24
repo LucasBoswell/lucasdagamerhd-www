9d9b37225bf591590a9896ced9574202070e33fc
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-04-24 03:11:03 -0600

    Fuck it, just defaulting. 3

--- html/index.php
+++ html/index.php
@@ -77,6 +77,7 @@ $(document).ready(function(){
 		var clientmsg = $("#usermsg").val();
 		$.post("post.php", {text: clientmsg});				
 		$("#usermsg").attr("value", "");
+		alert("test");
 		return false;
 	});
 	
