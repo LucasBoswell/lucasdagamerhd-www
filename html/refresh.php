3cc71a9dbc248627cedd35a8349da6c28db5a607
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-04-24 04:45:59 -0600

    Testing security.

--- html/refresh.php
+++ html/refresh.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 if(isset($_GET["key"])) {
 	if($_GET["key"] == "123") {
 		exec("sh refresh.sh");
-		echo "Refreshed.";
+		echo "Refreshed. Yay!";
 	}
 } else echo "Invalid password.";
 ?>
