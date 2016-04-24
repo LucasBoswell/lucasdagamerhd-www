7f35ced7f2aea72e16fd04c26669612b53b56d85
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-04-24 04:57:15 -0600

    Tweaks.

--- html/refresh.php
+++ html/refresh.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 if(isset($_GET["key"])) {
 	if($_GET["key"] == "123") {
-		exec("sh refresh.sh");
+		echo shell_exec("sh refresh.sh");
 		echo "Refreshed. Yay!";
 	}
 } else echo "Invalid password.";
