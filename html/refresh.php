0ab74c09d8ac387e2a0df11925075f56d8630078
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-04-24 04:44:55 -0600

    Security fixed.

--- html/refresh.php
+++ html/refresh.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-if(isset($_POST["key"])) {
-	if($_POST["key"] == "123") {
+if(isset($_GET["key"])) {
+	if($_GET["key"] == "123") {
 		exec("sh refresh.sh");
 		echo "Refreshed.";
 	}
