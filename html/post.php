2529d8ab5a0231cdd550be196b99fb0efa4b60ce
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-04-24 03:33:27 -0600

    Fuck it, just defaulting. 6

--- html/post.php
+++ html/post.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 session_start();
 if(isset($_SESSION['name'])){
 	$text = $_POST['text'];
-	echo($text);
+	echo($_POST['text']);
 	$fp = fopen("log.html", 'a');
 	fwrite($fp, "<div class='msgln'>(".date("g:i A").") <b>".$_SESSION['name']."</b>: ".stripslashes(htmlspecialchars($text))."<br></div>");
 	fclose($fp);
