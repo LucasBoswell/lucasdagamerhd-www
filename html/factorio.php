5b25ea3818b142b0d82b140acf03e55af2f6a586
JosiahBoswell <josiah.s.boswell@gmail.com>
2016-05-05 17:40:59 -0600

    Factorio mod test

---
+++ html/factorio.php
@@ -0,0 +1,21 @@
+<?php
+
+
+?>
+<table>
+<tr>
+<th>Mod Version</th>
+<th>Mod Title/Link</th>
+<th>Mod Download</th>
+</tr>
+<tr>
+<td>1.2.3</td>
+<td><a href="https://forums.factorio.com/viewtopic.php?f=91&t=19458&hilit=8l+train+project">8L Train Project</a></td>
+<td><a href="factoriomods/8L-Train-Project_1.2.3.zip">Downloady</a></td>
+</tr>
+<tr>
+<td>0.2.13</td>
+<td><a href="https://forums.factorio.com/viewtopic.php?f=92&t=14388">Advanced Logistics System</a></td>
+<td><a href="factoriomods/advanced-logistics-system_0.2.13.zip">Downloady</a></td>
+</tr>
+</table>
